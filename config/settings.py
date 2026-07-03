a7561e7bc232a933ade1869522fa8dee4df7026c
Anatoly Pavlinin <anatolianych@gmail.com>
2026-07-03 18:52:01 +0300

    settings redis

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,9 @@ EMAIL_HOST_USER = 'rider3766@yandex.ru'
 EMAIL_HOST_PASSWORD = 'kwuojvwcpfydxbgd'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
